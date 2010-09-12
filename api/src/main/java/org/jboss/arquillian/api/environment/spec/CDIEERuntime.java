dbf5079ed49e4bcc4e53efcc103d82d20bd47ca6
Dan Allen <dan.j.allen@gmail.com>
2010-09-12 03:07:26 -0400

    remove deprecated specs/rules

--- api/src/main/java/org/jboss/arquillian/api/environment/spec/CDIEERuntime.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.arquillian.api.environment.spec;
-
-import org.jboss.arquillian.api.environment.ExecutionEnvironment;
-import org.jboss.arquillian.api.environment.ProvidedBy;
-
-@ProvidedBy(
-   containers = "~ org\\.jboss\\.arquillian\\.container\\.weld\\.ee\\..*",
-   environments = JavaEE6Container.class)
-public final class CDIEERuntime implements ExecutionEnvironment {}
--- api/src/main/java/org/jboss/arquillian/api/environment/spec/EJB3_1Runtime.java
+++
@@ -1,7 +0,0 @@
-package org.jboss.arquillian.api.environment.spec;
-
-import org.jboss.arquillian.api.environment.ExecutionEnvironment;
-import org.jboss.arquillian.api.environment.ProvidedBy;
-
-@ProvidedBy(environments = JavaEE6Container.class)
-public final class EJB3_1Runtime implements ExecutionEnvironment {}
