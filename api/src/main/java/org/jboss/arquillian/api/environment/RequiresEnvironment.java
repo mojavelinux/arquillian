02b9b6356ec902d61cb26a4c84da862ea72d73de
Dan Allen <dan.j.allen@gmail.com>
2010-09-15 04:07:54 -0400

    comment

--- api/src/main/java/org/jboss/arquillian/api/environment/RequiresEnvironment.java
+++ api/src/main/java/org/jboss/arquillian/api/environment/RequiresEnvironment.java
@@ -6,16 +6,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
 /**
- * An annotation used to declare which execution environment an integration
- * test requires to function properly.
- *
- * <p>One or more configuration identifiers can be specified to require only
- * specific configurations of an execution environment. This may be useful if
- * the different configurations effectively create unique execution
- * environments. Container configurations are defined using the Arquillian
- * container configuration mechanism (i.e., arquillian.xml).</p>
+ * An annotation used to declare which execution environment an integration test
+ * requires to function properly.
+ * 
+ * <p>
+ * A configuration identifier can be specified to require only a specific
+ * configurations of an execution environment. This may be useful if the
+ * different configurations effectively create unique execution environments.
+ * Container configurations are defined using the Arquillian container
+ * configuration mechanism (i.e., arquillian.xml).
+ * </p>
  * 
  * @author Dan Allen
  */
